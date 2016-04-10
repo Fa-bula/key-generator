f7aae1ecc6f92e25b0ad3e551aa825844d608aa2
bulat <fa_bula@mail.ru>
2016-04-10 21:41:38 +0300

    added templates and forms to choose key to repay or check status

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import sqlite3
 import string
 import random
 
+TOTAL_NUMBER_OF_KEYS = (len(string.letters + string.digits)) ** 4
 app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
 
@@ -23,6 +24,12 @@ def init_db():
 
 def string_generator(size=4, chars=string.letters + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
+
+
+def get_remain_keys_number():
+    g.db = connect_db()
+    cur = g.db.execute('select count(*) from given_keys')
+    return TOTAL_NUMBER_OF_KEYS - cur.fetchall()[0][0]
 
 
 @app.before_request
@@ -47,21 +54,34 @@ def get_key():
             break
         except sqlite3.IntegrityError:
             pass
-    return key
+    return render_template('index.html', key=key,
+                           remain=get_remain_keys_number())
 
 
 @app.route('/status/<key>')
-def get_status(key):
+def getStatus(key):
     cur = g.db.execute('select is_repaid from given_keys where val=?', [key])
     result = cur.fetchall()
-    print(result)
     if result == []:
-        return 'не выдан'
+        status = 'еще не выдан'
     else:
-        if result == 1:
-            return 'погашен'
+        if result[0][0] == 1:
+            status = 'уже выдан и погашен'
         else:
-            return 'выдан'
+            status = 'уже выдан, но еще не погашен'
+    return render_template('status.html', key=key,
+                           keyStatus=status.decode('utf-8'),
+                           remain=get_remain_keys_number())
+
+
+@app.route('/status/', methods=['GET', 'POST'])
+def chooseKeyToGetStatus():
+    if request.method == 'POST':
+        key = request.form['key']
+        return redirect(url_for('getStatus', key=key))
+
+    return render_template('status_choose.html',
+                           remain=get_remain_keys_number())
 
 
 @app.route('/repay/<key>')
@@ -69,14 +89,30 @@ def repay(key):
     cur = g.db.execute('select is_repaid from given_keys where val=?', [key])
     result = cur.fetchall()
     if result == []:
-        return 'Такой ключ еще не выдавался'
-    is_repaid = result[0][0]
-    if is_repaid == 1:
-        return 'Этот ключ был погашен ранее'
-    if is_repaid == 0:
-        cur = g.db.execute('update given_keys set is_repaid=1 where val=?', [key])
-        g.db.commit()
-        return 'Ключ успешно погашен'
+        status = ', но такой ключ еще не выдавался'
+    else:
+        is_repaid = result[0][0]
+        if is_repaid == 1:
+            status = ', но этот ключ был погашен ранее'
+        if is_repaid == 0:
+            cur = g.db.execute('update given_keys set is_repaid=1 where val=?',
+                               [key])
+            g.db.commit()
+            status = ' и этот ключ успешно погашен'
+    return render_template('repay.html', key=key,
+                           keyStatus=status.decode('utf-8'),
+                           remain=get_remain_keys_number())
+
+
+@app.route('/repay/', methods=['GET', 'POST'])
+def chooseKeyToRepay():
+    if request.method == 'POST':
+        key = request.form['key']
+        return redirect(url_for('repay', key=key))
+
+    return render_template('repay_choose.html',
+                           remain=get_remain_keys_number())
+
 
 if __name__ == '__main__':
     app.run()
